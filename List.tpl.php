e4bc5a4edd34e7fc42dd648efcd5b8310eecbe03
Nigel Banks <nigel.g.banks@gmail.com>
2011-05-25 12:02:48 -0300

    Rendering form, basics for database back storage.

---
+++ List.tpl.php
@@ -0,0 +1,15 @@
+<?php ?>
+<div id="content-model-form-main">
+  <div id="content-model-form-table">
+    <table>
+      <tr><th>Content Model</th><th>Form</th><th>Remove Association</th></tr>
+      <?php foreach ($list as $row) : ?>
+        <?php list($id, $model, $form_name) = $row; ?>
+      <tr><td><?php print $model ?></td><td><?php print $form_name ?></td><td><?php print "<a href='/admin/content/model/forms/remove/$id'>Delete</a>"?></td></tr>
+      <?php endforeach; ?>
+    </table>
+  </div>
+  <div id="content-model-actions">
+    <?php print $form ?>
+  </div>
+</div>
