46876e877b1292f3c6523aa0d373e93dc6d99d34
Nigel Banks <nigel.g.banks@gmail.com>
2011-07-06 22:06:14 -0300

    Hooked more form fields to the data base.

--- List.tpl.php
+++ List.tpl.php
@@ -2,10 +2,10 @@
 <div id="content-model-form-main">
   <div id="content-model-form-table">
     <table>
-      <tr><th>Content Model</th><th>Form</th><th>Datastream ID</th><th>Remove Association</th></tr>
+      <tr><th>Content Model</th><th>Datastream ID</th><th>Title Field</th><th>Form</th><th>Has Template</th><th>Remove Association</th></tr>
       <?php foreach ($list as $row) : ?>
-        <?php list($id, $model, $form_name, $dsid) = $row; ?>
-      <tr><td><?php print $model ?></td><td><?php print $form_name ?></td><td><?php print $dsid  ?></td><td><?php print "<a href='/admin/content/model/forms/remove/$id'>Delete</a>"?></td></tr>
+        <?php list($id, $model, $form_name, $dsid, $title_field, $has_template) = $row; ?>
+      <tr><td><?php print $model ?></td><td><?php print $dsid ?></td><td><?php print $title_field ?></td><td><?php print $form_name ?></td><td><?php print ($has_template) ? 'True' : 'False' ?></td><td><?php print "<a href='/admin/content/model/forms/remove/$id'>Delete</a>"?></td></tr>
       <?php endforeach; ?>
     </table>
   </div>
