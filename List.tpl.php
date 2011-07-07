d27daeeb8d886b924e8c604bd84f36dffd59a197
Nigel Banks <nigel.g.banks@gmail.com>
2011-07-07 07:16:35 -0300

    Added transforms to the form associations.

--- List.tpl.php
+++ List.tpl.php
@@ -2,10 +2,26 @@
 <div id="content-model-form-main">
   <div id="content-model-form-table">
     <table>
-      <tr><th>Content Model</th><th>Datastream ID</th><th>Title Field</th><th>Form</th><th>Has Template</th><th>Remove Association</th></tr>
+      <tr>
+        <th>Content Model</th>
+        <th>Datastream ID</th>
+        <th>Title Field</th>
+        <th>Form</th>
+        <th>Transform</th>
+        <th>Has Template</th>
+        <th>Remove Association</th>
+      </tr>
       <?php foreach ($list as $row) : ?>
-        <?php list($id, $model, $form_name, $dsid, $title_field, $has_template) = $row; ?>
-      <tr><td><?php print $model ?></td><td><?php print $dsid ?></td><td><?php print $title_field ?></td><td><?php print $form_name ?></td><td><?php print ($has_template) ? 'True' : 'False' ?></td><td><?php print "<a href='/admin/content/model/forms/remove/$id'>Delete</a>"?></td></tr>
+        <?php list($id, $model, $form_name, $dsid, $title_field, $transform, $has_template) = $row; ?>
+      <tr>
+        <td><?php print $model ?></td>
+        <td><?php print $dsid ?></td>
+        <td><?php print $title_field ?></td>
+        <td><?php print $form_name ?></td>
+        <td><?php print $transform ?></td>
+        <td><?php print ($has_template) ? 'True' : 'False' ?></td>
+        <td><?php print "<a href='/admin/content/model/forms/remove/$id'>Delete</a>"?></td>
+      </tr>
       <?php endforeach; ?>
     </table>
   </div>
