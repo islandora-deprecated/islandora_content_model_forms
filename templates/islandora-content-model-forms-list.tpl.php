e2dcbeccd2d3f3e526c50937e00a05990c041586
DannyJoris <danny_joris@yahoo.co.uk>
2012-09-11 16:15:02 -0300

    renamed template file and wrapped strings in t() functions

--- templates/islandora-content-model-forms-list.tpl.php
+++ templates/islandora-content-model-forms-list.tpl.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  *
@@ -9,13 +10,13 @@
   <div id="content-model-form-table">
     <table>
       <tr>
-        <th>Content Model</th>
-        <th>Datastream ID</th>
-        <th>Title Field</th>
-        <th>Form</th>
-        <th>Transform</th>
-        <th>Has Template</th>
-        <th>Remove Association</th>
+        <th><?php print t('Content model'); ?></th>
+        <th><?php print t('Datastream ID'); ?></th>
+        <th><?php print t('Title field'); ?></th>
+        <th><?php print t('Form'); ?></th>
+        <th><?php print t('Transform'); ?></th>
+        <th><?php print t('Has template'); ?></th>
+        <th><?php print t('Remove association'); ?></th>
       </tr>
       <?php foreach ($list as $row) : ?>
       <?php list($id, $model, $form_name, $dsid, $title_field, $transform, $has_template) = $row; ?>
@@ -25,7 +26,7 @@
           <td><?php print $title_field ?></td>
           <td><?php print $form_name ?></td>
           <td><?php print $transform ?></td>
-          <td><?php print ($has_template) ? 'True' : 'False' ?></td>
+          <td><?php print ($has_template) ? t('Yes') : t('No') ?></td>
           <td><?php print l(t("Delete"), "admin/islandora/model/forms/remove/$id") ?></td>
         </tr>
       <?php endforeach; ?>
